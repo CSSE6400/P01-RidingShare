bfdfd1aa82204217f5d2b93a43485e41c5ef7108
Shristi Gupta <68809622+shristigupta1408@users.noreply.github.com>
2024-05-05 14:42:48 +0000

    Added basic cards for available ride requests.

--- application/app/frontend/src/App.js
+++ application/app/frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PassengerManager from './components/PassengerManager';
 import './App.css';
+import RideRequests from './components/RideRequests';
 
 /**
  * App component - the main component of the application.
@@ -14,6 +15,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<PassengerManager />} />
+        <Route path="/rides" element={<RideRequests />} />
       </Routes>
     </Router>
   );
