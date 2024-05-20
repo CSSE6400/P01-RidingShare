251d5ae34e94094b05aa2eaff56d06452b01a0b3
Mohamad Dabboussi <114811082+Mohamad11Dab@users.noreply.github.com>
2024-05-20 14:40:49 +0000

    leaflet routing

--- application/app/frontend/src/components/Map.js
+++ application/app/frontend/src/components/Map.js
@@ -1,20 +1,22 @@
 import React, { useRef } from "react";
-import { MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
-import L from 'leaflet';
+import { MapContainer, TileLayer} from "react-leaflet";
+
 import "leaflet/dist/leaflet.css";
 
-// Import the marker icon image
-import markerIconPng from "leaflet/dist/images/marker-icon.png"
 import 'leaflet/dist/images/marker-shadow.png';
+import Routing from "./Routing";
+
+const locations = [
+  { lat: -27.4679, lng: 153.0215, name: "Start Point" },
+  { lat: -27.4705, lng: 153.0265, name: "Mid Point" },
+  { lat: -27.4517, lng: 153.0412, name: "End Point" }
+];
 
 const SimpleMap = () => {
   const mapRef = useRef(null);
   const latitude = -27.4679;
   const longitude = 153.0215;
 
-  const customIcon = new L.Icon({
-    iconUrl: markerIconPng,
-  });
 
   return ( 
     <MapContainer center={[latitude, longitude]} zoom={13} ref={mapRef} style={{height: "100vh", width: "100vw"}}>
@@ -22,11 +24,7 @@ const SimpleMap = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[latitude, longitude]} icon={customIcon}>
-        <Popup>
-          Let's finish this assignment already
-        </Popup>
-      </Marker>
+      <Routing waypoints={locations} />
     </MapContainer>
   );
 };
