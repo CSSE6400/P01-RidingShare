6cdfc0202a08c715106122906dcd946a1733770b
Henry Batt <37820016+henrybatt@users.noreply.github.com>
2024-05-26 10:59:59 +0000

    Terraform to deploy the application + database + loadbalancer
    The DNS is not linked to certificate correctly due to DNS rules

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,20 @@
 data "aws_iam_role" "lab" { 
-   name = "LabRole" 
+    name = "LabRole" 
 } 
  
 data "aws_vpc" "default" { 
-   default = true 
+    default = true 
 } 
  
 data "aws_subnets" "private" { 
-   filter { 
-      name = "vpc-id" 
-      values = [data.aws_vpc.default.id] 
-   } 
+    filter { 
+        name = "vpc-id" 
+        values = [data.aws_vpc.default.id] 
+    } 
+}
+
+
+output "public_dns_name" {
+    description = "Public DNS address of the load balancer listener"
+    value 		= aws_lb.riding_share.dns_name
 }
