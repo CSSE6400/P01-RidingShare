46179578fdba623601678f8c572d59e438e21d6b
Mohamad Dabboussi <114811082+Mohamad11Dab@users.noreply.github.com>
2024-05-26 07:49:31 +0000

    added script for testing

---
+++ unittest.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Navigate to the application directory where docker-compose.yml is located
+cd application
+
+# Start only the database service
+docker-compose up -d database
+
+# Wait for 10 seconds to allow the database to initialize
+echo "Waiting for the database service to initialize... Sleeping for 15 seconds"
+sleep 15
+
+# Navigate to the tests directory and run unittests
+echo "Running tests..."
+cd app/tests
+python -m unittest discover
+
+# Navigate back to the application directory
+echo "Shutting down services..."
+cd ../../..
+
+# Shut down all services
+./cleanup.sh
+./purge_db.sh
+echo "Testing and cleanup completed."
